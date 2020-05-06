3b6207c0d6389c4b3e71d77785dc1bcb5de80e41
Renan Bertoldo <renan.rbertoldo@gmail.com>
2020-05-06 19:49:26 -0300

    feat: Added debug configuration

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
-import express, { request, response } from 'express';
+import express from 'express';
 import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
 app.use(routes);
 
 app.listen(3333, () => {

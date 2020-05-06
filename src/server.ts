2180e085cd19b41d68c4dfbd2f6616142ffaf1c8
Renan Bertoldo <renan.rbertoldo@gmail.com>
2020-05-06 19:03:11 -0300

    feat: ESLint config added

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
-import express, { request, response } from "express";
+import express, { request, response } from 'express';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello GoStack" });
-});
+app.get('/', (request, response) => response.json({ message: 'Hello GoStack' }));
 
 app.listen(3333, () => {
-  console.log("🎧️ Server stated on port 3333");
+  console.log('🎧️ Server stated on port 3333');
 });

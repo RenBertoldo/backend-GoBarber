30d23c8e3a84bc2360509f286ac2771630bd23d9
Renan Bertoldo <renan.rbertoldo@gmail.com>
2020-05-08 02:01:52 -0300

    feat: Creation of avatar update route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUserService from '../services/CreatUserService';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -21,5 +26,16 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  (request, response) => {
+    console.log(request.file);
+
+    return response.json({ ok: true });
+  },
+);
 
 export default usersRouter;

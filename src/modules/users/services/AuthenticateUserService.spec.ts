62d696de5f3a301c54032dda872c6f1a9976de44
Renan Bertoldo <renan.rbertoldo@gmail.com>
2020-05-22 17:51:43 -0300

    feat: Created to test auth of user

---
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -0,0 +1,35 @@
+// import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
+import AuthenticateUserService from './AuthenticateUserService';
+import CreateUserService from './CreateUserService';
+
+describe('AuthenticateUser', () => {
+  it('should be able to authenticate', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'jhondoe@example.com',
+      password: '123456',
+    });
+
+    const reposponse = await authenticateUser.execute({
+      email: 'jhondoe@example.com',
+      password: '123456',
+    });
+
+    expect(reposponse).toHaveProperty('token');
+    expect(reposponse.user).toEqual(user);
+  });
+});

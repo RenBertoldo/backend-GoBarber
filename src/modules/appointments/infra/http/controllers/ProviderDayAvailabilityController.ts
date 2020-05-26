60730dc0e80876c596a8bf1ee9f7759ca1a964ec
Renan Bertoldo <renan.rbertoldo@gmail.com>
2020-05-26 11:01:09 -0300

    fix: Changed of body to query params

--- src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -6,16 +6,16 @@ import ListProviderDayAvailabilityService from '@modules/appointments/services/L
 export default class ProviderDayAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
     const { provider_id } = request.params;
-    const { year, month, day } = request.body;
+    const { year, month, day } = request.query;
 
     const listProviderDayAvailability = container.resolve(
       ListProviderDayAvailabilityService,
     );
 
     const availability = await listProviderDayAvailability.execute({
-      day,
-      month,
-      year,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
       provider_id,
     });
 
